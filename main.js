21d9104f04ddc9dc3ceb743a4762b979d821c745
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2023-12-28 15:30:31 +0100

    update

--- main.js
+++ main.js
@@ -5,42 +5,45 @@ var Notifplace = document.getElementById("NotifBox")
 var showpastext = document.getElementById("showpasswordtext")
 var myloader = document.getElementById("loading")
 
-let mylogin =  [{
-    Username : "Admin",
-    Password : "Admin12"
-},{
-    Username :"User",
-    Password :"User12"
-},{
-    Username :"User3",
-    Password :"User123"
-}
+let mylogin =  [
+    {Username : "Admin" , Password  : "Admin12" , isAdmin : true},
+    {Username : "User"  , Password  : "User12",   isAdmin : false},
+    {Username : "User3" , Password  : "User123",  isAdmin : false}
 ]
 
 
 signinbtn.addEventListener("click",()=>{
-    // event.preventDefault()
-    // for(let i = 0 ; i<=mylogin.length;i++){
-    //     if(myUsername.value === mylogin[i].Username && myPassword.value === mylogin[i].Password){
-    //         if(mylogin[i].Username === "Admin"){
-    //             Succesmsg("Success",`Vous etes connecte Mr ${mylogin[i].Username}`)
-    //             myloader.style.display = 'flex'
-    //             setTimeout(()=>{
-    //                 // window.open("adminpage.html","_self")
-    //             },4000)
-    //         }
-    //         else{
-    //             Succesmsg("Success",`Vous etes connecte Mr ${mylogin[i].Username}`)
-    //             myloader.style.display = 'flex'
-    //             setTimeout(()=>{
-    //                 // window.open("page2.html","_self")
-    //             },4000)
-    //         }
-    //     }
-    //     else{
-    //         Errormsg("Error","Username ou password invalid")
-    //     }
-    // }
+    event.preventDefault()
+    if(myUsername.value !== "" || myPassword.value !== "" ){
+        let Exist = false;
+        for(let i = 0 ; i<mylogin.length;i++){
+            if(myUsername.value === mylogin[i].Username && myPassword.value === mylogin[i].Password){
+                Exist = true
+                if(mylogin[i].isAdmin == true){
+                    var role = "Admin"
+                }
+                break;
+            }
+        }
+        if(Exist && role == "Admin"){
+            Succesmsg("Success",`Vous etes connecte ${myUsername.value}`)
+            myloader.style.display = 'flex'
+            setTimeout(()=>{
+                window.open("adminpage.html","_self")
+            },4000)
+        }else if(Exist){
+            Succesmsg("Success",`Vous etes connecte ${myUsername.value}`)
+            myloader.style.display = 'flex'
+            setTimeout(()=>{
+                window.open("page2.html","_self")
+            },4000)
+        }else{
+            Errormsg("Error","Username ou password Incorrect")
+
+        }
+    }else{
+        Errormsg("Error","Username ou password vide")
+    }
 })
 
 showpastext.addEventListener("click",()=>{
