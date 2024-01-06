ad5556f3a164da3cebbcf15230c88eef1bd33c32
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2024-01-06 15:51:46 +0100

    x

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+
 var myUsername = document.getElementById("user")
 var myPassword = document.getElementById("pass")
 var signinbtn = document.getElementById("signin")
 var Notifplace = document.getElementById("NotifBox")
-var showpastext = document.getElementById("showpasswordtext")
+var showpastext = document.querySelectorAll(".showtext")
 var myloader = document.getElementById("loading")
+var forgpass = document.getElementById("forgetpass")
+var allpassinputs = document.querySelectorAll("input#pass")
 
 let mylogin =  [
     {Username : "Admin" , Password  : "Admin12" , isAdmin : true},
@@ -44,20 +47,34 @@ signinbtn.addEventListener("click",()=>{
     }else{
         Errormsg("Error","Username ou password vide")
     }
+    console.log(myUsername.value)
 })
 
-showpastext.addEventListener("click",()=>{
-    event.preventDefault()
-    if(myPassword.type == "password"){
-        myPassword.type = "text"
-        showpasswordtext.innerHTML = "hide" 
-    }else{
-        myPassword.type = "password" 
-        showpasswordtext.innerHTML = "Show" 
-    }
-})
+
+// showpastext.addEventListener("click",()=>{
+//     event.preventDefault()
+//     allpassinputs.forEach((input)=>{
+//         if(input.type == "password"){
+//             input.type = "text"
+//             showpastext.innerHTML = "hide" 
+//         }else{
+//             input.type = "password" 
+//             showpastext.innerHTML = "Show" 
+//         }
+//     })
+// })
+
+// forgpass.addEventListener("click",()=>{
+//     event.preventDefault()
+//     localStorage.setItem("Usernamereset",myUsername.value)
+//     window.open("passwordreset.html","_self")
+// })
+
+
 
 function Errormsg(title,msg){
+    event.preventDefault()
+
     let nf = document.createElement('div')
     nf.classList.add('NotifError')
     nf.innerHTML = `<div class = "flexi"><h2>${title}</h2><i class="fa-solid fa-circle-info"></i></div>
@@ -69,6 +86,8 @@ function Errormsg(title,msg){
 }
 
 function Succesmsg(title,msg){
+    event.preventDefault()
+
     let nf = document.createElement('div')
     nf.classList.add('NotifSuccess')
     nf.innerHTML = `<div class = "flexi"><h2>${title}</h2><i class="fa-solid fa-circle-info"></i></div>
